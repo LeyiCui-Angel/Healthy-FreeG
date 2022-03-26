a82dd5bb57113473bea5f7f2e2dc266957ece5b9
tianqizhao-louis <louiszhao0517@gmail.com>
2022-03-26 06:04:40 -0400

    new commit: set up

--- misc.py
+++ misc.py
@@ -4,3 +4,11 @@ conn = sqlite3.connect('freeg.db')
 
 c = conn.cursor()
 
+# c.execute("""insert into expiration ("item_name", "item_exp") values("banana", 3)""")
+
+c.execute("""SELECT * from expiration""")
+print(c.fetchall())
+
+conn.commit()
+
+conn.close()
